4f4ef93971cbc73059581e7bae1ad86d43f52a21
vijay <sahanivijay800@gmail.com>
2020-12-12 18:12:04 +0530

    working great

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ letters=[chr(i) for i in range(97,123)]
 arr=[(index,item) for index,item in enumerate(letters)]
 key=randint(2,5)
 
-
+print(key)
 def ceasor_Cipher(text):
     '''
     Adding key into the text which will decide the encrption of your text
@@ -19,8 +19,6 @@ def ceasor_Cipher(text):
                     nums.append(pos+key)
         Cipher_text.append(nums)
     return encrpyt(Cipher_text)
-
-
 
 
 def encrpyt(num):
@@ -45,7 +43,36 @@ def encrpyt(num):
         enc_text.append(text)
     return " ".join(enc_text)   
 
+print(ceasor_Cipher(input(">")))
 
 
+def decrypt_text(text,key):
+    diCipher_text,text=[],text.lower().split(" ")
 
-print(ceasor_Cipher("My name is vijay")) 
+    for i in text:
+        nums=[]
+        for j in i:
+            for pos,letter in arr:
+                if letter==j:
+                    nums.append(pos-key)
+        diCipher_text.append(nums)
+    new=[]
+    for ind in diCipher_text:
+        corr=[]
+        for i in ind:
+            if i<0:
+                i=26+i
+            corr.append(i)
+        new.append(corr)
+    dec_text=[]
+    for grp in new:
+        text=""
+        for n in grp:
+            for index,item in arr:
+                if n==index:
+                    text+=item
+        dec_text.append(text)
+    
+    return " ".join(dec_text)
+
+print(decrypt_text(input(">"),key))
